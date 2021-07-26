7a549d41d09473a64d880c7c50c645b2436e8a39
Anmol Sehgal <idx2882@gmail.com>
2021-07-26 21:40:53 +0530

    Converting functional component to class component of ListComponent

--- src/components/ListCardComponents.tsx
+++ src/components/ListCardComponents.tsx
@@ -1,19 +1,35 @@
-import {FC,ReactElement} from 'react';
-import { AppStateInterface,stateObj } from '../App';
-import CardComponent  from './CardComponent';
+import { Component } from "react";
+import { ReactElement } from "react";
+import { AppStateInterface, stateObj } from "../App";
+import CardComponent from "./CardComponent";
 
-export interface ListCardInterface{
-    data:AppStateInterface;
-    handleChange :  (content:string,objectID:string)=>void;
-    handleDelete : (objectID:string)=>void;
-    handleActive: (inactive:boolean,objectID:string)=>void;
+export interface ListCardInterface {
+  data: AppStateInterface;
+  handleChange: (content: string, objectID: string) => void;
+  handleDelete: (objectID: string) => void;
+  handleActive: (inactive: boolean, objectID: string) => void;
 }
 
-const ListCardComponents:FC<ListCardInterface> = ({data,handleChange,handleDelete,handleActive}:ListCardInterface):ReactElement=>{
-    return(<ul style={{listStyle:"none"}}>
-            {data.data.map((value:stateObj):ReactElement=>{
-                return  <CardComponent tag="li" data = {value} handleChange={handleChange} key={value.objectID} handleDelete= {handleDelete} keyValue={value.objectID} handleActive={handleActive}/>
-            })}
-    </ul>)
+class ListCardComponents extends Component<ListCardInterface, {}> {
+  render() {
+    const { data, handleChange, handleDelete, handleActive } = this.props;
+    return (
+      <ul style={{ listStyle: "none" }}>
+        {data.data.map((value: stateObj): ReactElement => {
+          return (
+            <CardComponent
+              tag="li"
+              data={value}
+              handleChange={handleChange}
+              key={value.objectID}
+              handleDelete={handleDelete}
+              keyValue={value.objectID}
+              handleActive={handleActive}
+            />
+          );
+        })}
+      </ul>
+    );
+  }
 }
 export default ListCardComponents;
